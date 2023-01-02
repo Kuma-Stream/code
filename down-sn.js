07515f7664253dcb3a3f5001bdd6fc6bc3de69ba
Kuma-Stream <121804937+Kuma-Stream@users.noreply.github.com>
2023-01-02 23:52:18 +0700

    Create down-sn.js

---
+++ down-sn.js
@@ -0,0 +1,14 @@
+/*snacklink*/
+var go_url = 'https://ponselharian.com/';
+    var api = 'eb07a1b2a5c7aa7befa32b22519e6cbe509f5088';
+    var shorten_includ = ["www.mirrored.to"];
+    
+ /*lm*/
+ var app_url = 'https://linkmonet.com/';
+                    var app_api_token = '37ff0be3e95fbc1b7b016577bc3d1b9afb3b3738';
+                    var app_advert = 2;
+                    var app_domains = ["terabox.com"];
+ 
+ /*ouo*/
+ var ouo_token = 'rrCvoHxA';
+    var domains = ['racaty.io', 'rapidshare.com', 'uploading.com', 'uploadable.ch', 'letitbit.net', 'turbobit.net'];
