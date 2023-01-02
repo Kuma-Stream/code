8a8769103ac102c67823726e99780cac9f5e5d02
Kuma-Stream <121804937+Kuma-Stream@users.noreply.github.com>
2023-01-03 03:03:40 +0700

    Update down-sn.js

--- down-sn.js
+++ down-sn.js
@@ -1,13 +1,13 @@
 /*snacklink*/
 var go_url = 'https://ponselharian.com/';
     var api = 'eb07a1b2a5c7aa7befa32b22519e6cbe509f5088';
-    var shorten_includ = ["www.mirrored.to", "https://to.menjelajahi.com"];
+    var shorten_includ = ["www.mirrored.to", "https://to.menjelajahi.com", "to.aoutoqw.xyz"];
     
  /*lm*/
  var app_url = 'https://linkmonet.com/';
                     var app_api_token = '37ff0be3e95fbc1b7b016577bc3d1b9afb3b3738';
                     var app_advert = 2;
-                    var app_domains = ["terabox.com"];
+                    var app_domains = ["terabox.com", "drive.google.com"];
  
  /*ouo*/
  var ouo_token = 'rrCvoHxA';
