abe78b0e142688d6332c7764ed642fead2798e0f
Kuma-Stream <121804937+Kuma-Stream@users.noreply.github.com>
2023-01-02 20:24:42 +0700

    Create down.php

---
+++ down.php
@@ -0,0 +1,51 @@
+<!-- [Box Download] -->
+<div class="resIn" id="Server-1">
+<div class="dbox-wrap">
+    <div class="dbox">
+<div class="dbox-title">Download Stellar | Episode 1</div>
+<div class="dbox-list">
+<strong>MP4</strong>
+<a href="#" rel="nofollow" target="_blank" title="Server 1">360p</a> |
+<a href="#" rel="nofollow" target="_blank" title="Server 2">480p</a> |
+<a href="#" rel="nofollow" target="_blank" title="Server 3">720p</a> |
+<a href="#" rel="nofollow" target="_blank" title="Server 4">1080</a> 
+</div>
+<div class="dbox-list">
+<strong>MKV</strong>
+<a href="#" rel="nofollow" target="_blank" title="Server 1">360p</a> |
+<a href="#" rel="nofollow" target="_blank" title="Server 2">480p</a> |
+<a href="#" rel="nofollow" target="_blank" title="Server 3">720p</a> |
+<a href="#" rel="nofollow" target="_blank" title="Server 4">1080</a> 
+</div>
+<div class="dbox-list">
+<strong>x265</strong>
+<a href="#" rel="nofollow" target="_blank" title="Server 1">360p</a> |
+<a href="#" rel="nofollow" target="_blank" title="Server 2">480p</a> |
+<a href="#" rel="nofollow" target="_blank" title="Server 3">720p</a> |
+<a href="#" rel="nofollow" target="_blank" title="Server 4">1080</a> 
+</div>
+</div>
+<div class="dbox">
+<div class="dbox-title">Download Batch</div>
+<div class="dbox-list">
+<strong>480p</strong>
+<a href="#" rel="nofollow" target="_blank" title="Server 1"> MKV </a> |
+<a href="#" rel="nofollow" target="_blank" title="Server 2"> MP4 </a> |
+<a href="#" rel="nofollow" target="_blank" title="Server 3"> x265 </a>
+</div>
+<div class="dbox-list">
+<strong>720p</strong>
+<a href="#" rel="nofollow" target="_blank" title="Server 1"> MKV </a> |
+<a href="#" rel="nofollow" target="_blank" title="Server 2"> MP4 </a> |
+<a href="#" rel="nofollow" target="_blank" title="Server 3"> x265 </a>
+</div>
+<div class="dbox-list">
+<strong>1080p</strong>
+<a href="#" rel="nofollow" target="_blank" title="Server 1"> MKV </a> |
+<a href="#" rel="nofollow" target="_blank" title="Server 2"> MP4 </a> |
+<a href="#" rel="nofollow" target="_blank" title="Server 3"> x265 </a>
+</div>
+</div>
+
+</div>
+</div>
